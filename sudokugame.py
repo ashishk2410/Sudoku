e8206a7e3079c632abc7e087b41b0f0de4e07da4
ashishk2410 <ashishk.dubey@gmail.com>
2025-03-08 23:41:04 +0530

    Update sudokugame.py

--- sudokugame.py
+++ sudokugame.py
@@ -19,12 +19,12 @@ def print_sudoku(puzzle):
 def pop_n(puzzle_comb, x, y, n):
     """Removes n from all related rows, columns, and boxes."""
     for j in range(GRID_SIZE):
-        if puzzle_comb[j][y] != 0 and n in puzzle_comb[j][y]:
+        if puzzle_comb[j][y] and n in puzzle_comb[j][y]:
             puzzle_comb[j][y].remove(n)
             if not puzzle_comb[j][y]:
                 puzzle_comb[j][y] = 0
 
-        if puzzle_comb[x][j] != 0 and n in puzzle_comb[x][j]:
+        if puzzle_comb[x][j] and n in puzzle_comb[x][j]:
             puzzle_comb[x][j].remove(n)
             if not puzzle_comb[x][j]:
                 puzzle_comb[x][j] = 0
@@ -33,7 +33,7 @@ def pop_n(puzzle_comb, x, y, n):
     box_y = (y // SUBGRID_SIZE) * SUBGRID_SIZE
     for j in range(box_x, box_x + SUBGRID_SIZE):
         for k in range(box_y, box_y + SUBGRID_SIZE):
-            if puzzle_comb[j][k] != 0 and n in puzzle_comb[j][k]:
+            if puzzle_comb[j][k] and n in puzzle_comb[j][k]:
                 puzzle_comb[j][k].remove(n)
                 if not puzzle_comb[j][k]:
                     puzzle_comb[j][k] = 0
@@ -61,7 +61,7 @@ def solve_sudoku(puzzle, puzzle_comb):
     flg = 0
     for x in range(GRID_SIZE):
         for y in range(GRID_SIZE):
-            if puzzle_comb[x][y] != 0 and len(puzzle_comb[x][y]) == 1:
+            if puzzle_comb[x][y] and len(puzzle_comb[x][y]) == 1:
                 n = puzzle_comb[x][y][0]
                 sudoku_cnt = input_n(puzzle, puzzle_comb, x, y, n, 81)
                 flg = 1
@@ -73,12 +73,12 @@ def naked_pair(puzzle_comb):
     for x in range(GRID_SIZE):
         s = -1
         for y in range(GRID_SIZE):
-            if s == -1 and puzzle_comb[x][y] != 0 and len(puzzle_comb[x][y]) == 2:
+            if s == -1 and puzzle_comb[x][y] and len(puzzle_comb[x][y]) == 2:
                 s = y
             elif s != -1 and puzzle_comb[x][y] == puzzle_comb[x][s]:
                 for item in puzzle_comb[x][y]:
                     for z in range(GRID_SIZE):
-                        if z != y and z != s and puzzle_comb[x][z] != 0 and item in puzzle_comb[x][z]:
+                        if z != y and z != s and puzzle_comb[x][z] and item in puzzle_comb[x][z]:
                             puzzle_comb[x][z].remove(item)
                             flg = 1
                 break
@@ -86,12 +86,12 @@ def naked_pair(puzzle_comb):
     for x in range(GRID_SIZE):
         s = -1
         for y in range(GRID_SIZE):
-            if s == -1 and puzzle_comb[y][x] != 0 and len(puzzle_comb[y][x]) == 2:
+            if s == -1 and puzzle_comb[y][x] and len(puzzle_comb[y][x]) == 2:
                 s = y
             elif s != -1 and puzzle_comb[y][x] == puzzle_comb[s][x]:
                 for item in puzzle_comb[y][x]:
                     for z in range(GRID_SIZE):
-                        if z != y and z != s and puzzle_comb[z][x] != 0 and item in puzzle_comb[z][x]:
+                        if z != y and z != s and puzzle_comb[z][x] and item in puzzle_comb[z][x]:
                             puzzle_comb[z][x].remove(item)
                             flg = 1
                 break
@@ -103,10 +103,9 @@ def hidden_pair_rc(puzzle_comb, choice):
     flg = 0
     for x in range(GRID_SIZE):
         srtd_box = []
-        srtd_box1 = []
         for t in range(GRID_SIZE):
             val = puzzle_comb[x][t] if choice == "r" else puzzle_comb[t][x]
-            if val != 0:
+            if val:
                 srtd_box += val
         srtd_box1 = sorted(set([i for i in srtd_box if srtd_box.count(i) == 2]))
         if len(srtd_box1) >= 2:
@@ -117,9 +116,9 @@ def hidden_pair_rc(puzzle_comb, choice):
             s = -1
             for y in range(GRID_SIZE):
                 r, c = (x, y) if choice == "r" else (y, x)
-                if s == -1 and puzzle_comb[r][c] != 0 and set(srtd_box1).intersection(puzzle_comb[r][c]) == set(srtd_box1):
+                if s == -1 and puzzle_comb[r][c] and set(srtd_box1).intersection(puzzle_comb[r][c]) == set(srtd_box1):
                     s = y
-                elif s != -1 and puzzle_comb[r][c] != 0 and set(srtd_box1).intersection(puzzle_comb[r][c]) == set(srtd_box1):
+                elif s != -1 and puzzle_comb[r][c] and set(srtd_box1).intersection(puzzle_comb[r][c]) == set(srtd_box1):
                     for item in puzzle_comb[r][c]:
                         if item not in srtd_box1:
                             puzzle_comb[r][c].remove(item)
@@ -165,7 +164,7 @@ def solve_sudoku_pos(puzzle_comb):
     flag = 0
     for x in range(GRID_SIZE):
         for y in range(GRID_SIZE):
-            if puzzle_comb[x][y] != 0 and len(puzzle_comb[x][y]) >= 1:
+            if puzzle_comb[x][y] and len(puzzle_comb[x][y]) >= 1:
                 for item in puzzle_comb[x][y]:
                     flag += solve_sudoku_pos_rc(puzzle_comb, x, y, item, "r")
                     flag += solve_sudoku_pos_rc(puzzle_comb, x, y, item, "c")
@@ -176,17 +175,16 @@ def find_unique_box(puzzle, puzzle_comb):
     flg = 0
     for x in range(0, GRID_SIZE, SUBGRID_SIZE):
         box = [[] for _ in range(SUBGRID_SIZE)]
-        srtd_box = [[] for _ in range(SUBGRID_SIZE)]
         for y in range(GRID_SIZE):
             for i in range(SUBGRID_SIZE):
-                if puzzle_comb[x + i][y] != 0:
+                if puzzle_comb[x + i][y]:
                     box[y // SUBGRID_SIZE] += puzzle_comb[x + i][y]
         for y in range(SUBGRID_SIZE):
-            srtd_box[y] = sorted(set([i for i in box[y] if box[y].count(i) == 1]))
-            for item in srtd_box[y]:
+            srtd_box = sorted(set([i for i in box[y] if box[y].count(i) == 1]))
+            for item in srtd_box:
                 for z in range(y * SUBGRID_SIZE, (y + 1) * SUBGRID_SIZE):
                     for i in range(SUBGRID_SIZE):
-                        if puzzle_comb[x + i][z] != 0 and puzzle_comb[x + i][z].count(item) == 1:
+                        if puzzle_comb[x + i][z] and puzzle_comb[x + i][z].count(item) == 1:
                             sudoku_cnt = input_n(puzzle, puzzle_comb, x + i, z, item, 81)
                             flg = 1
                             break
@@ -197,17 +195,16 @@ def solve_candidate_line(puzzle, puzzle_comb):
     flg = 0
     for x in range(0, GRID_SIZE, SUBGRID_SIZE):
         box = [[] for _ in range(SUBGRID_SIZE)]
-        srtd_box = [[] for _ in range(SUBGRID_SIZE)]
         for y in range(GRID_SIZE):
             for i in range(SUBGRID_SIZE):
-                if puzzle_comb[x + i][y] != 0:
+                if puzzle_comb[x + i][y]:
                     box[y // SUBGRID_SIZE] += puzzle_comb[x + i][y]
         for y in range(SUBGRID_SIZE):
-            srtd_box[y] = sorted(set([i for i in box[y] if box[y].count(i) >= 1]))
-            for item in srtd_box[y]:
+            srtd_box = sorted(set([i for i in box[y] if box[y].count(i) >= 1]))
+            for item in srtd_box:
                 for z in range(y * SUBGRID_SIZE, (y + 1) * SUBGRID_SIZE):
                     for i in range(SUBGRID_SIZE):
-                        if puzzle_comb[x + i][z] != 0 and puzzle_comb[x + i][z].count(item) == 1:
+                        if puzzle_comb[x + i][z] and puzzle_comb[x + i][z].count(item) == 1:
                             flg += unique_in_row(puzzle_comb, item, x + i, z)
                             flg += unique_in_col(puzzle_comb, item, x + i, z)
     return flg
@@ -238,24 +235,23 @@ def find_unique(puzzle, puzzle_comb):
     """Finds unique candidates in rows and columns and assigns them."""
     flg = 0
     for x in range(GRID_SIZE):
-        lst_row = []
-        lst_col = []
+        lst_row, lst_col = [], []
         for y in range(GRID_SIZE):
-            if puzzle_comb[x][y] != 0:
+            if puzzle_comb[x][y]:
                 lst_row += puzzle_comb[x][y]
-            if puzzle_comb[y][x] != 0:
+            if puzzle_comb[y][x]:
                 lst_col += puzzle_comb[y][x]
         srtd_row = sorted(set([i for i in lst_row if lst_row.count(i) == 1]))
         srtd_col = sorted(set([i for i in lst_col if lst_col.count(i) == 1]))
         for item in srtd_row:
             for y in range(GRID_SIZE):
-                if puzzle_comb[x][y] != 0 and puzzle_comb[x][y].count(item) == 1:
+                if puzzle_comb[x][y] and puzzle_comb[x][y].count(item) == 1:
                     sudoku_cnt = input_n(puzzle, puzzle_comb, x, y, item, 81)
                     flg = 1
                     break
         for item in srtd_col:
             for y in range(GRID_SIZE):
-                if puzzle_comb[y][x] != 0 and puzzle_comb[y][x].count(item) == 1:
+                if puzzle_comb[y][x] and puzzle_comb[y][x].count(item) == 1:
                     sudoku_cnt = input_n(puzzle, puzzle_comb, y, x, item, 81)
                     flg = 1
                     break
@@ -263,12 +259,11 @@ def find_unique(puzzle, puzzle_comb):
 
 def main():
     """Main function to solve the Sudoku puzzle."""
-    w, h, z = GRID_SIZE, GRID_SIZE, GRID_SIZE
     sudoku_cnt = 81
     puzzle = [[0 for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
     puzzle_comb = [[[x + 1 for x in range(GRID_SIZE)] for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
     s_inputs = [122, 155, 177, 233, 264, 298, 337, 361, 426, 439, 453, 512, 558, 597, 652, 676, 684, 743, 771, 819, 846, 872, 938, 951, 986]
-    
+
     puzzle, puzzle_comb = input_sudoku(puzzle, puzzle_comb, s_inputs)
     print("Sudoku sheet")
     print_sudoku(puzzle)
@@ -352,7 +347,7 @@ def main():
 
         for i1 in range(GRID_SIZE):
             for j1 in range(GRID_SIZE):
-                if temp_comb[i1][j1] != 0:
+                if temp_comb[i1][j1]:
                     x, y = i1, j1
                     for item in temp_comb[x][y]:
                         print("attempt", i + 1)
@@ -374,20 +369,22 @@ def main():
                             sudoku_cnt = temp_count
                             puzzle = deepcopy(temp_puzzle)
                             puzzle_comb = deepcopy(temp_comb)
-                        i=temp_i
-                if sudoku_cnt<=0: 
+                        i = temp_i
+                if sudoku_cnt <= 0:
                     break
-    elif sudoku_cnt<=0 and level==0:
-        level=5
+    elif sudoku_cnt <= 0 and level == 0:
+        level = 5
 
-    if(sudoku_cnt>0):
+    if sudoku_cnt > 0:
         print("No Solution")
         print_sudoku(puzzle)
         print_sudoku(puzzle_comb)
-        print("Does this has one solution??")
-    
+        print("Does this have one solution??")
     else:
-        print("Solved.. puzzle level=",level)
+        print("Solved.. puzzle level=", level)
         print_sudoku(puzzle)
-        end_time=datetime.datetime.now()
-        print("Elapsed Time ",end_time-st_time)
+        end_time = datetime.datetime.now()
+        print("Elapsed Time ", end_time - st_time)
+
+if __name__ == "__main__":
+    main()
